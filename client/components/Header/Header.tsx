9e7dcfa225e80fdc0a4597203147ec10d6788155
Alone <tranluong460@gmail.com>
2023-12-13 03:13:00 +0700

    fix

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -59,7 +59,7 @@ const Header = () => {
             </div>
 
             <div className="ml-auto flex items-center">
-              <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+              <div className="lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                 <UserMenu />
               </div>
 
