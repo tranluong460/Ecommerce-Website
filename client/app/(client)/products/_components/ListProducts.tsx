0eea15544500c597c7ba3da15a19131cf5daedb9
Alone <tranluong460@gmail.com>
2023-12-14 21:54:14 +0700

    fix

--- client/app/(client)/products/_components/ListProducts.tsx
+++ client/app/(client)/products/_components/ListProducts.tsx
@@ -8,7 +8,7 @@ type ListProductsProps = {
 const ListProducts = ({ products }: ListProductsProps) => {
   return (
     <>
-      {products && products.length < 0 ? (
+      {products && products.length > 0 ? (
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
             <ProductItem key={product._id} product={product} />
