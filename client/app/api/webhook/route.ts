67838cdb718dc5bd3b7d4539e2be62d2c746080a
Alone <tranluong460@gmail.com>
2023-12-16 06:11:36 +0700

    fix

--- client/app/api/webhook/route.ts
+++ client/app/api/webhook/route.ts
@@ -1,6 +1,9 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { createUser } from "@/app/actions/user";
 
 export async function POST(req: Request) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
@@ -51,8 +54,31 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+  if (eventType === "user.created") {
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    };
+
+    const newUser = await createUser(user);
+
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
+
+    return NextResponse.json({ message: "OK", user: newUser });
+  }
 
   return new Response("", { status: 200 });
 }
