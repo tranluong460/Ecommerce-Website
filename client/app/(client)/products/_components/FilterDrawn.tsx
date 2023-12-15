e5412f2a4554a0a8574ddc44e7ab827ac024fcf7
Alone <tranluong460@gmail.com>
2023-12-15 18:49:32 +0700

    fix

--- client/app/(client)/products/_components/FilterDrawn.tsx
+++ client/app/(client)/products/_components/FilterDrawn.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import {
   Sheet,
   SheetContent,
@@ -6,17 +9,21 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { Separator } from "@/components/ui/separator";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { filter__links } from "@/data/links";
 
 const FilterDrawn = () => {
+  const [isOpen, setIsOpen] = useState<{ [key: string]: boolean }>({
+    sex: true,
+    size: true,
+  });
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -43,26 +50,65 @@ const FilterDrawn = () => {
           <Separator className="my-4" />
         </SheetHeader>
 
-        <Accordion type="multiple" className="w-full">
-          {filter__links.map((item) => (
-            <AccordionItem key={item.name} value={item.name}>
-              <AccordionTrigger>{item.label}</AccordionTrigger>
-
-              {item.children.map((children) => (
-                <AccordionContent key={children.name}>
-                  <div className="flex flex-col gap-2">
-                    <div className="flex gap-2">
-                      <Checkbox id={`${item.name}-${children.name}`} />
-                      <Label htmlFor={`${item.name}-${children.name}`}>
-                        {children.label}
-                      </Label>
-                    </div>
+        {filter__links.map((item) => (
+          <Collapsible
+            key={item.name}
+            open={isOpen[item.name]}
+            onOpenChange={() =>
+              setIsOpen((prev) => ({ ...prev, [item.name]: !prev[item.name] }))
+            }
+            className="border-t dark:border-secondary px-4 py-6"
+          >
+            <CollapsibleTrigger className="-mx-2 -my-3 flow-root w-full">
+              <div className="flex items-center justify-between px-2 py-3 text-muted-foreground hover:text-primary">
+                <span className="font-medium">{item.label}</span>
+                <span className="ml-6 flex items-center">
+                  {isOpen[item.name] ? (
+                    <svg
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                    </svg>
+                  )}
+                </span>
+              </div>
+            </CollapsibleTrigger>
+            {item.children.map((children) => (
+              <CollapsibleContent key={children.name} className="pt-6">
+                <div className="space-y-6">
+                  <div className="flex items-center">
+                    <Checkbox
+                      id={`${item.name}-${children.name}`}
+                      className="h-4 w-4 rounded border dark:border-secondary text-primary focus:ring-primary"
+                    />
+                    <Label
+                      htmlFor={`${item.name}-${children.name}`}
+                      className="ml-3 min-w-0 flex-1 text-muted-foreground"
+                    >
+                      {children.label}
+                    </Label>
                   </div>
-                </AccordionContent>
-              ))}
-            </AccordionItem>
-          ))}
-        </Accordion>
+                </div>
+              </CollapsibleContent>
+            ))}
+          </Collapsible>
+        ))}
       </SheetContent>
     </Sheet>
   );
