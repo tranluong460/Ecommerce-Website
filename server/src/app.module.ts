3a504d7019bafd5b2c2b510faf77a55b6ab230b7
Alone <tranluong460@gmail.com>
2023-12-16 02:18:51 +0700

    fix

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.DB_URL),
     UsersModule,
   ],
   controllers: [],
