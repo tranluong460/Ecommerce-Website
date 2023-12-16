271b3f61c390080111ef82f465700f8b6240fbbe
Alone <tranluong460@gmail.com>
2023-12-16 07:44:37 +0700

    fix

--- client/app/api/webhooks/clerk/route.ts
+++ client/app/api/webhooks/clerk/route.ts
@@ -65,17 +65,13 @@ export async function POST(req: Request) {
       photo: image_url,
     };
 
-    const res = await fetch("http://localhost:8080/users", {
+    await fetch("http://localhost:8080/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
     });
-
-    const data = await res.json();
-
-    return Response.json(data);
   }
 
   return new Response("", { status: 200 });
