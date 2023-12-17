11c3dd483d2754a4a01f622f346b89f792aff7e9
Alone <tranluong460@gmail.com>
2023-12-17 10:38:25 +0700

    fix

--- client/app/(client)/products/[...slug]/_components/ImageGallery.tsx
+++ client/app/(client)/products/[...slug]/_components/ImageGallery.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import { IImageProduct } from "@/interface/products";
 
@@ -6,14 +9,17 @@ type ImageGalleryProps = {
 };
 
 const ImageGallery = ({ images }: ImageGalleryProps) => {
+  const [selectImage, setSelectImage] = useState("");
+
   return (
     <div className="flex flex-col-reverse">
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
         <div className="grid grid-cols-4 gap-6">
           {images.map((image) => (
-            <div
+            <button
               key={image.url}
-              className="relative flex h-24 cursor-default items-center justify-center rounded-md bg-background text-sm font-medium uppercase text-foreground hover:bg-background/80 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+              onClick={() => setSelectImage(image.url)}
+              className="relative flex h-24 items-center justify-center rounded-md bg-background text-sm font-medium uppercase text-foreground hover:bg-background/80 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
             >
               <span className="absolute inset-0 overflow-hidden rounded-md">
                 <Image
@@ -24,7 +30,7 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
                   className="h-full w-full object-cover object-center"
                 />
               </span>
-            </div>
+            </button>
           ))}
         </div>
       </div>
@@ -33,8 +39,8 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
         <Image
           width={1000}
           height={1000}
-          src={images[0].url || ""}
-          alt={images[0].url || ""}
+          src={selectImage || images[0].url}
+          alt={selectImage || images[0].url}
           className="h-full w-full object-cover object-center sm:rounded-lg"
         />
       </div>
