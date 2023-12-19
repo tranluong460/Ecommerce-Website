e423d76380608e3d6d49232b3260552859abc024
Alone <tranluong460@gmail.com>
2023-12-19 21:43:08 +0700

    fix be

--- server/src/comments/comments.module.ts
+++ server/src/comments/comments.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { Comment, CommentSchema } from './entities/comment.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+  ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- server/src/comments/comments.service.ts
+++ server/src/comments/comments.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comment } from './entities/comment.entity';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectModel(Comment.name) private commentModel: Model<Comment>,
+  ) {}
+
+  async create(createCommentDto: CreateCommentDto): Promise<Comment> {
+    const createComment = new this.commentModel(createCommentDto);
+
+    if (!createComment) {
+      throw new NotFoundException('Thêm mới bình luận thất bại!');
+    }
+
+    return createComment.save();
   }
 
-  findAll() {
-    return `This action returns all comments`;
+  async findAll(): Promise<Comment[]> {
+    const allComments = await this.commentModel.find().exec();
+    return allComments;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} comment`;
+  async findOne(id: string): Promise<Comment> {
+    const oneComment = await this.commentModel.findById(id).exec();
+
+    if (!oneComment) {
+      throw new NotFoundException('Không có thông tin bình luận!');
+    }
+
+    return oneComment;
   }
 
-  update(id: string, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
+  async update(
+    id: string,
+    updateCommentDto: UpdateCommentDto,
+  ): Promise<Comment> {
+    const updateComment = await this.commentModel
+      .findByIdAndUpdate(id, { $set: updateCommentDto }, { new: true })
+      .exec();
+
+    if (!updateComment) {
+      throw new NotFoundException('Cập nhật bình luận thất bại!');
+    }
+
+    return updateComment;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} comment`;
+  async remove(id: string) {
+    const removeComment = await this.commentModel.findByIdAndDelete(id).exec();
+
+    if (!removeComment) {
+      throw new NotFoundException('Xóa bình luận thất bại!');
+    }
+
+    return removeComment;
   }
 }
