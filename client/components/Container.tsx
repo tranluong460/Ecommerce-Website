45503724e8336ec748c4cf31cd4d60c836435994
Alone <tranluong460@gmail.com>
2023-12-21 01:56:15 +0700

    up

--- client/components/Container.tsx
+++ client/components/Container.tsx
@@ -1,9 +1,11 @@
+import "@/styles/other/components.css";
+
 type ContainerProps = {
   children: React.ReactNode;
 };
 
 const Container = ({ children }: ContainerProps) => {
-  return <div className="px-5 max-w-[1280px] mx-auto">{children}</div>;
+  return <div className="cr-div">{children}</div>;
 };
 
 export default Container;
--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import "@/styles/other/components.css";
 
 const Logo = () => {
   return (
-    <div className="font-semibold text-2xl text-foreground">
+    <div className="lo-div">
       <Link href="/">
         <Image
           width={1000}
           height={1000}
           src="/logo.png"
           alt="Logo"
-          className="w-20 h-14"
+          className="lo-image"
           priority
         />
       </Link>
