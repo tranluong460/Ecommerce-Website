ab25629c6e0a74abcddff1d947bbb914a595098e
Alone <tranluong460@gmail.com>
2023-12-20 20:45:06 +0700

    fix

--- client/libs/products.ts
+++ client/libs/products.ts
@@ -2,7 +2,8 @@ export const getAllProducts = async () => {
   const response = await fetch(
     process.env.NEXT_ENV === "deployment"
       ? "http://localhost:8080/products"
-      : "https://seines.vercel.app/products"
+      : "https://seines.vercel.app/products",
+    { cache: "no-cache" }
   );
 
   return response.json();
@@ -12,7 +13,8 @@ export const getOneProduct = async (id: string) => {
   const response = await fetch(
     process.env.NEXT_ENV === "deployment"
       ? `http://localhost:8080/products/${id}`
-      : `https://seines.vercel.app/products/${id}`
+      : `https://seines.vercel.app/products/${id}`,
+    { cache: "no-cache" }
   );
   return response.json();
 };
