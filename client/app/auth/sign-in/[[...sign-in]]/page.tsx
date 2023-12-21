3ebce01d21fcc1a4cd0201f0fb05aecff7618e31
Alone <tranluong460@gmail.com>
2023-12-21 23:27:11 +0700

    fix

--- client/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ client/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -10,7 +10,7 @@ export const generateMetadata = async (): Promise<Metadata> => {
 
 const SignInPage = () => {
   return (
-    <section className="max-w-screen-xl m-0 sm:m-6 sm:rounded-lg flex justify-center flex-1 bg-background border dark:border-secondary">
+    <section className="max-w-screen-xl m-0 sm:m-6 sm:rounded-lg flex justify-center flex-1 bg-background border dark:border-secondary min-h-screen">
       <SignInForm />
 
       <div className="flex-1 text-center hidden lg:flex">
--- client/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ client/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -10,7 +10,7 @@ export const generateMetadata = async (): Promise<Metadata> => {
 
 const RegisterPage = () => {
   return (
-    <section className="max-w-screen-xl m-0 sm:m-3 sm:rounded-lg flex justify-center flex-1 bg-background border dark:border-secondary">
+    <section className="max-w-screen-xl m-0 sm:m-3 sm:rounded-lg flex justify-center flex-1 bg-background border dark:border-secondary min-h-screen">
       <SignUpForm />
 
       <div className="flex-1 text-center hidden lg:flex">
